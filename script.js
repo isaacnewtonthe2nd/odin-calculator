5d1b20d06088672aeb7530894d9108a45f0e2e12
isaacnewtonthe2nd <ayotuns73@gmail.com>
2024-11-16 20:58:19 -0800

    Display numbers when you click on the each corresponding buttons and put a limit on the amount of numbers.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let displayContainer = document.querySelector(".display-container");
-displayContainer.style.cssText = `width: ${500 - 40}px; height: ${(560 / 6)}px; padding: 5px; margin-bottom: 10px; background-color: grey;`;
+displayContainer.style.cssText = `width: ${500 - 40}px; height: ${(560 / 6)}px; padding: 8px; margin-bottom: 10px; background-color: grey; font-size: 70px; text-align: right; color: black;`;
+displayContainer.value = '0';
 
 let numeroUno;
 let numeroDos;
@@ -33,6 +34,18 @@ function multiplyNumbers(...nums) {
 
 function divideNumbers(...nums) {
   return nums.reduce((total, current) => {return total /= current});
+}
+
+function numberDisplay(strNum) {
+  if (Number(displayContainer.value) <= 10e9) {
+    if (displayContainer.value === '0') {
+      return displayContainer.value = strNum;
+    } else {
+      return displayContainer.value += strNum;
+    }
+  } else {
+    return displayContainer.value;
+  }
 }
 
 // console.log(operate(operator, numeroUno, numeroDos));
