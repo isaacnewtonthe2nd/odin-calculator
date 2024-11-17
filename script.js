5ce47d0371154e382e2348e0498d356fb18508fc
isaacnewtonthe2nd <ayotuns73@gmail.com>
2024-11-16 16:02:15 -0800

    Create 3 variables to store an operator and 2 numbers and a function Operate to use those variables to solve.

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let numeroUno;
+let numeroDos;
+let operator;
+
+function operate (operator, num1, num2) {
+  if (operator === '+') {
+    return addNumbers(num1, num2);
+  } else if (operator === '-') {
+    return subtractNumbers(num1, num2);
+  } else if (operator === '*') {
+    return multiplyNumbers(num1, num2);
+  } else if (operator === '/') {
+    return divideNumbers(num1, num2);
+  } else {
+    return num1;
+  }
+}
+
 function addNumbers(...nums) {
   return nums.reduce((total, current) => { return total += current}, 0);
 }
@@ -13,3 +31,5 @@ function multiplyNumbers(...nums) {
 function divideNumbers(...nums) {
   return nums.reduce((total, current) => {return total /= current});
 }
+
+// console.log(operate(operator, numeroUno, numeroDos));
